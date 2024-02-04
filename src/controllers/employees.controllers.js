bcc1dd6a99dda1b88c73a3c0f11e0ac3c5992583
JuanCarlosGP <juancarlosgp212@gmail.com>
2024-02-04 21:24:09 +0100

    Gestión de errores

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -2,58 +2,79 @@ import { pool } from '../db.js'
 
 
 export const getEmployees = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM employee')
-    console.log(rows)
-    res.json('Obteniendo empleados')
+    try {
+        const [rows] = await pool.query('SELECT * FROM employee')
+        res.json(rows)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
 export const getEmployee = async (req, res) => {
-    //console.log(req.params.id)
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-
-    if (rows.length <= 0) return res.status(404).json({
-        message: 'Employee not found'
-    })
-
-    res.json(rows[0])
+    try {
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+        if (rows.length <= 0) return res.status(404).json({
+            message: 'Employee not found'
+        })
+        res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
 
 export const createEmployees = async (req, res) => {
-    // Datos en formato json
-    // {
-    // "name": "Mario",
-    // "salary": 2700
-    // }
-    const { name, salary } = req.body
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-    // Test para comprobar que llegan datos de json con thunderClient
-    // console.log(req.body)
-    res.send({
-        id: rows.insertId,
-        name,
-        salary
-    })
+    const { name, salary } = req.body 
+    try {
+        // Datos en formato json
+        // {
+        // "name": "Mario",
+        // "salary": 2700
+        // }
+        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+        // Test para comprobar que llegan datos de json con thunderClient
+        // console.log(req.body)
+        res.send({
+            id: rows.insertId,
+            name,
+            salary
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
 
 export const deleteEmployees = async (req, res) => {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
-
-    if (result.affectedRows <= 0) return res.status(404).json({
-        message: 'Employee not found'
-    })
-    res.sendStatus(204)
-
+    try {
+        const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
+        if (result.affectedRows <= 0) return res.status(404).json({
+            message: 'Employee not found'
+        })
+        res.sendStatus(204)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }
 
 export const updateEmployees = async (req, res) => {
     const { id } = req.params
     const { name, salary } = req.body
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
-
-    if (result.affectedRows === 0) return res.status(404).json({
-        message: 'Employee not found'
-    })
-
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
-
-    res.json([rows])
+    try {
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL(?, salary) WHERE id = ?', [name, salary, id])
+        if (result.affectedRows <= 0) return res.status(404).json({
+            message: 'Employee not found'
+        })
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+        res.json(rows[0])
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error'
+        })
+    }
 }

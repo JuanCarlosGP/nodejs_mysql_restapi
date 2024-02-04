0ca0d436ac6ad5087063a47ace6ad0f21d384e3d
JuanCarlosGP <juancarlosgp212@gmail.com>
2024-02-04 20:14:06 +0100

    GET especificando datos

--- src/controllers/employees.controllers.js
+++ src/controllers/employees.controllers.js
@@ -3,8 +3,18 @@ import { pool } from '../db.js'
 
 export const getEmployees = async(req, res)=>{
     const [rows] = await pool.query('SELECT * FROM employee')
-    res.json(rows)
-} 
+    console.log(rows)
+    res.json('Obteniendo empleados')
+}
+export const getEmployee = async(req, res)=> {
+    //console.log(req.params.id)
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+    
+    if (rows.length <= 0) return res.status(404).json({
+        message: 'Employee not found'
+    })
+    
+    res.json(rows[0])}
 
 export const createEmployees = async (req, res) => {
     const { name, salary } = req.body
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getEmployees, createEmployees, updateEmployees, deleteEmployees } from "../controllers/employees.controllers.js";
+import { getEmployees, createEmployees, updateEmployees, deleteEmployees, getEmployee } from "../controllers/employees.controllers.js";
 
 const router = Router()
 
 router.get('/employees', getEmployees)
+router.get('/employees/:id', getEmployee)
+
 router.post('/employees', createEmployees)
 router.put('/employees', updateEmployees)
 router.delete('/employees', deleteEmployees)

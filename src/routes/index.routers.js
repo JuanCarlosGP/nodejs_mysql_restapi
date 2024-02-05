69c116e1bb3671dad02e49db57219142d9f43373
JuanCarlosGP <juancarlosgp212@gmail.com>
2024-02-05 17:45:13 +0100

    test railway

--- src/routes/index.routers.js
+++ src/routes/index.routers.js
@@ -4,6 +4,6 @@ import { ping } from "../controllers/index.controllers.js";
 const router = Router();
 
 // Test de conexion a la base de datos
-router.get('/test', ping)
+router.get('/ping', ping)
 
 export default router
